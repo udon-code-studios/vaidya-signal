6d413a02772d260d0fc11e8b3d9a5c06c1bbe35c
Leo <leo.battalora@gmail.com>
2023-08-18 14:56:48 -0400

    add charting

--- vaidya-signal-ui/app/page.tsx
+++ vaidya-signal-ui/app/page.tsx
@@ -1,15 +1,15 @@
-import Image from "next/image";
+import CandlestickChart from "@/components/CandlestickChart";
 
 export default function Home() {
   return (
     <div className="w-full flex flex-col gap-4">
-      <p className="pt-16 text-center text-5xl font-bold">Vaidya Signal</p>
-      <div className="p-6 font-serif">
+      <p className="pt-12 pb-4 text-center text-5xl font-bold">Vaidya Signal</p>
+      <div className="px-6 font-serif">
         <p className="pb-4">
           The Vaidya Signal is a specific bottom reversal divergence signal
           which is triggered when the following conditions are met:
         </p>
-        <p className="pl-4">
+        <p className="pl-3">
           <strong>1.</strong>{" "}
           the current low* (L2) is lower than the previous low (L1)
         </p>
@@ -17,13 +17,17 @@ export default function Home() {
           *a low is defined as having 3 days before and after whith higher
           closes
         </p>
-        <p className="pt-2 pl-4">
+        <p className="pt-1 pl-4">
           <strong>2.</strong>{" "}
           MACD and RSI are both higher at L2 than they were at L1
         </p>
-        <p className="pt-2 pl-4">
+        <p className="pt-1 pl-4">
           <strong>3.</strong> volume at the L2 is lower than it was at L1
         </p>
+      </div>
+      <div className="-mx-10 my-5 border-t border-gray-700" />
+      <div className="px-2">
+        <CandlestickChart ticker="SPY"/>
       </div>
     </div>
   );
---
+++ vaidya-signal-ui/components/CandlestickChart.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import Highcharts from "highcharts/highstock";
+import HighchartsReact from "highcharts-react-official";
+
+export default function CandlestickChart(props: { ticker: string }) {
+  const options: Highcharts.Options = {
+    title: {
+      text: props.ticker,
+    },
+    series: [{
+      type: "candlestick",
+      data: [
+        [Date.UTC(2022, 5, 2), 5, 9, 4, 7],
+        [Date.UTC(2022, 5, 3), 7, 16, 7, 15],
+        [Date.UTC(2022, 5, 4), 9, 12, 3, 5],
+      ],
+    }, {
+      type: "line",
+      name: "Close",
+      data: [[Date.UTC(2022, 5, 2), 7], [Date.UTC(2022, 5, 3), 15], [Date.UTC(2022, 5, 4), 5]],
+    }],
+    xAxis: {
+      type: "datetime",
+      plotLines: [{
+        color: "red", // Color of the line
+        width: 2, // Width of the line
+        value: Date.UTC(2022, 5, 3), // X-axis value where the line should be placed
+        zIndex: 5, // Z-index of the line, to make sure it appears above the candlestick series
+      }],
+    },
+  };
+
+  return (
+    <div className="w-full">
+      <HighchartsReact
+        highcharts={Highcharts}
+        options={options}
+      />
+    </div>
+  );
+}
