d1fce935556b6ad782ebfd3159a8a4791214ff2f
Leo <leo.battalora@gmail.com>
2023-08-27 19:43:54 -0400

    updated db for extra indicator fields

--- database/supabase_ddl.sql
+++ database/supabase_ddl.sql
@@ -1,5 +1,5 @@
 create
-or replace function create_days_ticker_table () returns trigger as $$
+or replace function create_ticker_tables () returns trigger as $$
 begin
   execute 'CREATE TABLE IF NOT EXISTS ' || 'days_' || replace(NEW.ticker, '.', '_') || ' (
     date date PRIMARY KEY NOT NULL,
@@ -9,7 +9,11 @@ begin
     close real NULL,
     volume integer NULL,
     macd real NULL,
-    rsi real NULL
+    rsi real NULL,
+    ema_12 real NULL,
+    ema_26 real NULL,
+    avg_gain real NULL,
+    avg_loss real NULL
   ) ';
   execute 'CREATE TABLE IF NOT EXISTS ' || 'vaidya_' || replace(NEW.ticker, '.', '_') || ' (
     trigger_date date PRIMARY KEY NOT NULL,
@@ -25,9 +29,10 @@ public.tickers (
   ticker text not null,
   first_date date not null,
   last_date date not null,
+  last_vaiday_signal_date date null,
   constraint tickers_pkey primary key (ticker)
 ) tablespace pg_default;
 
 create trigger insert_ticker
 after insert on tickers for each row
-execute function create_days_ticker_table ();
+execute function create_ticker_tables ();
