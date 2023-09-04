ac8d0a5515852a1516dff266f0c95bcb7521353e
Leo <leo.battalora@gmail.com>
2023-09-04 19:16:17 -0400

    error handling

--- vaidya-signal-ui/src/utils/vaidya-service.ts
+++ vaidya-signal-ui/src/utils/vaidya-service.ts
@@ -1,4 +1,12 @@
+import { sortWatchlistByLastTrigger } from "./pure";
+
 const vaidyaServiceUrl = import.meta.env.VAIDYA_SERVICE_URL;
+
+export interface SignalTrigger {
+  trigger_date: string;
+  low_2_date: string;
+  low_1_date: string;
+}
 
 export const getSignalTriggers = async (ticker: string) => {
   if (!ticker) {
@@ -8,6 +16,22 @@ export const getSignalTriggers = async (ticker: string) => {
   const res = await fetch(`${vaidyaServiceUrl}/api/v1/get-signal-triggers?ticker=${ticker}`);
 
   if (res.ok) {
-    return await res.json();
+    const json = (await res.json()) as [];
+
+    if (json.length === 0) {
+      return;
+    }
+
+    return json as SignalTrigger[];
   }
+};
+
+export const getMostRecentSignalTrigger = async (ticker: string) => {
+  const triggers = await getSignalTriggers(ticker);
+
+  if (!triggers) {
+    return;
+  }
+
+  return triggers[triggers.length - 1].trigger_date;
 };
