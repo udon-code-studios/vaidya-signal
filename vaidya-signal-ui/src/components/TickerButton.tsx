e8da10c598ffc25967b8863a73e58667ce3378b3
Leo <leo.battalora@gmail.com>
2023-09-04 16:17:59 -0400

    link to relevant ticker

--- vaidya-signal-ui/src/components/TickerButton.tsx
+++ vaidya-signal-ui/src/components/TickerButton.tsx
@@ -1,7 +1,7 @@
 export default function Search({ ticker = "QQQ" }: { ticker: string }) {
   return (
-    <a href="dog.com">
-      <div className="w-full flex justify-center border p-2 rounded-lg py-4 sm:py-2 hover:text-skin-accent hover:font-bold">
+    <a href={`/ticker/${ticker}`}>
+      <div className="w-full flex justify-center border p-2 rounded-lg py-4 sm:py-2 hover:text-skin-accent hover:font-bold hover:border-skin-accent">
         <p className="">{ticker}</p>
       </div>
     </a>
