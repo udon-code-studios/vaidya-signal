c2344af420d0beae7b01a34f97c98786d10c49a3
Leo <leo.battalora@gmail.com>
2023-09-04 16:15:06 -0400

    input bar to add tickers to watchlist

---
+++ vaidya-signal-ui/src/components/AddTickers.tsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { addTickersToWatchlist } from "../utils/supabase";
+
+export default function AddTickers() {
+  const [input, setInput] = useState<string>("");
+
+  const handleEnter = async (tickers: string) => {
+    if (tickers !== "") {
+      await addTickersToWatchlist(tickers.toLocaleUpperCase());
+      window.location.reload();
+    }
+  };
+
+  return (
+    <div className="w-full flex flex-col gap-2 px-4">
+      {/* input bar */}
+      <div className="flex gap-3">
+        <input
+          className="block flex-grow rounded-lg border border-skin-fill 
+        border-opacity-40 bg-skin-fill py-3 pl-4
+        pr-3 placeholder:italic placeholder:text-opacity-75 
+        focus:border-skin-accent focus:outline-none"
+          placeholder="QQQ BRK.B AAPL ..."
+          type="text"
+          name="search"
+          value={input}
+          onChange={(e) => setInput(e.currentTarget.value)}
+          autoComplete="off"
+          onKeyDown={(e) => {
+            if (e.key === "Enter" && input !== "") {
+              handleEnter(input);
+            }
+          }}
+        />
+        <button
+          className="bg-skin-accent text-skin-inverted px-4 rounded-lg font-bold hover:bg-skin-fill hover:text-skin-accent border-2 border-skin-accent"
+          onClick={() => handleEnter(input)}
+        >
+          ADD TICKERS
+        </button>
+      </div>
+
+      {/* instructions */}
+      <div>
+        <p className="text-xs italic font-bold text-center">
+          * Enter a space-separated list of tickers (e.g. "AAPL MSFT GOOG")
+        </p>
+      </div>
+    </div>
+  );
+}
