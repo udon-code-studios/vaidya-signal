b5cc58909db202892d1eea6cf8c83881fdb29d02
Leo <leo.battalora@gmail.com>
2023-09-10 20:13:46 -0400

    remove helpers and remove cron

--- vaidya-signal-service/src/main.go
+++ vaidya-signal-service/src/main.go
@@ -5,51 +5,36 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"time"
 
-	A "github.com/udon-code-sudios/vaidya-signal-service/api"
-	S "github.com/udon-code-sudios/vaidya-signal-service/service"
-
-	"github.com/go-co-op/gocron"
 	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/jmoiron/sqlx"
+	A "github.com/udon-code-sudios/vaidya-signal-service/api"
+	U "github.com/udon-code-sudios/vaidya-signal-service/utils"
 )
 
 func main() {
 	//--------------------------------------------------------------------------
-	// Ensure Alpaca environment variables are set
+	// Ensure environment variables are set
 	//--------------------------------------------------------------------------
 
-	checkAlpacaEnvVars()
+	envVars := []string{"APCA_API_KEY_ID", "APCA_API_SECRET_KEY", "PG_URI", "EMAIL_PW"}
+
+	for _, envVar := range envVars {
+		if os.Getenv(envVar) == "" {
+			fmt.Println("[ ERROR ] The environment variable", envVar, "is not set")
+			os.Exit(1)
+		}
+	}
 
 	//--------------------------------------------------------------------------
 	// Open PostgreSQL connection
 	//--------------------------------------------------------------------------
 
-	db := connectToPg()
+	db, err := sqlx.Connect("pgx", os.Getenv("PG_URI"))
+	U.PanicOnNotNil(err)
 	defer db.Close()
+
 	database := A.Database{DB: db}
-
-	//--------------------------------------------------------------------------
-	// Create cron job to print status every hour
-	//--------------------------------------------------------------------------
-
-	// create scheduler
-	newYork, err := time.LoadLocation("America/New_York")
-	panicOnNotNil(err)
-	scheduler := gocron.NewScheduler(newYork)
-
-	// define jobs
-	scheduler.Every(1).Hour().Do(func() {
-		fmt.Println("[ INFO ] Service is still running...")
-	})
-	scheduler.Every(1).Day().At("16:20").Do(func() {
-		fmt.Println("[ INFO ] Running daily watchlist scan...")
-		S.ScanWatchlist(db)
-	})
-
-	// start scheduler
-	scheduler.StartAsync()
 
 	//--------------------------------------------------------------------------
 	// Start HTTP server
@@ -66,81 +51,10 @@ func main() {
 	http.HandleFunc("/", A.UselessHandler)
 	http.HandleFunc("/hello", A.UselessHandler)
 	http.HandleFunc("/alive", A.UselessHandler)
-	// http.HandleFunc("/api/v1/add-ticker", database.AddTickerHandler)
-	// http.HandleFunc("/api/v1/remove-ticker", A.UselessHandler)
-	// http.HandleFunc("/api/v1/add-email", A.UselessHandler)
-	// http.HandleFunc("/api/v1/remove-email", A.UselessHandler)
 	http.HandleFunc("/api/v1/get-signal-triggers", A.GetVaidyaSignalsHandler)
 	http.HandleFunc("/api/v1/update-watchlist", database.UpdateWatchlistHandler)
+	http.HandleFunc("/api/v1/update-watchlist-email-today-triggers", database.UpdateWatchlistEmailTodayTriggersHandler)
 
 	// start server
 	log.Fatal(http.ListenAndServe(":"+port, nil))
-}
-
-//----------------------------------------------------------------------------
-// types
-//----------------------------------------------------------------------------
-
-// type DaysTable struct {
-// 	Date   time.Time `db:"date"`
-// 	Open   float64   `db:"open"`
-// 	High   float64   `db:"high"`
-// 	Low    float64   `db:"low"`
-// 	Close  float64   `db:"close"`
-// 	Volume uint64    `db:"volume"`
-// 	MACD   float64   `db:"macd"`
-// 	RSI    float64   `db:"rsi"`
-// }
-
-// type TickersTable struct {
-// 	Ticker    string `db:"ticker"`
-// 	FirstDate string `db:"first_date"`
-// 	LastDate  string `db:"last_date"`
-// }
-
-// type VaidyaSignalsTable struct {
-// 	TriggerDate time.Time `db:"trigger_date"` // day signal was triggered
-// 	Low2Date    time.Time `db:"low_2_date"`   // current low
-// 	Low1Date    time.Time `db:"low_1_date"`   // previous low
-// }
-
-//----------------------------------------------------------------------------
-// helper functions
-//----------------------------------------------------------------------------
-
-func panicOnNotNil(value interface{}) {
-	if value != nil {
-		panic(value)
-	}
-}
-
-/*
-Checks if all Alpaca environment variables are set, exiting with status 1 if
-not.
-*/
-func checkAlpacaEnvVars() {
-	alpacaEnvVars := []string{"APCA_API_KEY_ID", "APCA_API_SECRET_KEY"}
-
-	for _, envVar := range alpacaEnvVars {
-		if os.Getenv(envVar) == "" {
-			fmt.Println("[ ERROR ] The environment variable", envVar, "is not set")
-			os.Exit(1)
-		}
-	}
-}
-
-func connectToPg() *sqlx.DB {
-	pgConnectionStringEnvVar := "PG_URI"
-	pgConnectionString := os.Getenv(pgConnectionStringEnvVar)
-
-	if pgConnectionString == "" {
-		fmt.Println("[ ERROR ] The environment variable", pgConnectionStringEnvVar, "is not set")
-		os.Exit(1)
-	}
-
-	// connect to db
-	db, err := sqlx.Connect("pgx", pgConnectionString)
-	panicOnNotNil(err)
-
-	return db
 }
